b4d51559e0ee1cfbd5f0f05d812292e30baa6140
AbhinavRai2004 <raiabhinav.in@gmail.com>
2022-08-25 21:38:12 +0530

    basic intro

---
+++ Basic/intro.js
@@ -0,0 +1,34 @@
+//Javascript:JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions
+/**
+ * // First-Class Function: A programming language is said to have First-class functions if functions in that language are treated like other variables. 
+ * So the functions can be assigned to any other variable or passed as an argument 
+ * or can be returned by another function. 
+ * JavaScript treat function as a first-class-citizens.
+ */
+
+// Javascript - brenden eich- 1997
+
+//latest is ES2015/ES6
+// ES6 is also known as ECMAScript 2015 as it is released in 2015. 
+//There are two new ways to define variables that are let and const. 
+// An arrow function is a new feature introduced in ES6 by which we don’t require the function keyword to define the function.
+
+// Declarartion and Initialization
+// var x = 10
+// var x = "abhi"
+// console.log(x)
+
+// var x;
+// x=10;
+// x=20;
+// var x=30;
+
+// let x=10;
+// let x=30; here when u execute the code it will throw error(syntax error).
+//because we cant redeclared once the variable is declared already with let keyword
+//let:cant be redeclared but can be initialized again n again
+
+//const x;
+// x=10 while executing like this will error.
+// in case of const keyword Declarartion and Initializationare done at the sametime.like const a=10
+// and you cant redeclared and reinitialized
